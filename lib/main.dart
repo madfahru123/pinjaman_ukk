ebedb7e3bc40ecf9dbcb2aef6b21865f60348ecc
galihhh25 <email_github_galihhh25@gmail.com>
2026-01-17 14:48:54 +0700

    Initial commit Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: "https://uorsdxyybuxwbzliqvnm.supabase.co",
+    anonKey: 'sb_publishable_nHD9Ngh2o5TObRFhepqM4A_g9u02vjh',
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
